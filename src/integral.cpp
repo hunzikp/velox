afd8efe86a34820c53f73be23c55cfa798167763
mwip <matthias.weigand@protonmail.com>
2019-07-01 16:08:54 +0200

    modularized the code of integral focal mean. introduced functions for finding the disc kernel and determinig groupable boxes heavy commenting

--- src/integral.cpp
+++ src/integral.cpp
@@ -1,5 +1,6 @@
 #include <Rcpp.h>
 #include <cmath>
+#include <vector>
 using namespace Rcpp;
 
 // [[Rcpp::export]]
@@ -7,8 +8,6 @@ NumericMatrix integralRaster(NumericMatrix rasterband) {
 
   int rows = rasterband.nrow();
   int cols = rasterband.ncol();
-
-
 
   for (int r = 0; r < rows; r++){
     for (int c = 0; c < cols; c++){
@@ -29,9 +28,32 @@ NumericMatrix integralRaster(NumericMatrix rasterband) {
   return  rasterband;
 }
 
+
+void fillKernel(NumericMatrix kernel, int diameter){
+  // ancilliary function which uses the Pythagorean theorem to determine which
+  // cell of the disc shaped focal kernel is within the disc shaped
+
+  int radius = (diameter-1) / 2;
+  int centX = radius;
+  int centY = radius;
+  int dX, dY;
+  float dist;
+
+  for (int y = 0; y < diameter; y++) {
+    dY = abs(y - centY);
+    for (int x = 0; x < diameter; x++){
+      dX = abs(x - centX);
+      dist = sqrt(pow(dX, 2) + pow(dY, 2));
+      if(dist <= radius) {
+        kernel(x,y) = 1;
+      }
+    }
+  }
+}
+
 void fillFirstRows(int* firstRows, NumericMatrix kernel, int diameter){
   // group columns of kernel into boxes with same first and last row to make
-  // even more use of integral image
+  // even more use of the integral image by creating rectangular boxes
 
   int prevFirstRow;
   int currValue;
@@ -66,28 +88,65 @@ void fillFirstRows(int* firstRows, NumericMatrix kernel, int diameter){
   }
 }
 
-void fillKernel(NumericMatrix kernel, int diameter){
-  int radius = (diameter-1) / 2;
-  int centX = radius;
-  int centY = radius;
-  int dX, dY;
-  float dist;
 
-  for (int y = 0; y < diameter; y++) {
-    dY = abs(y - centY);
-    for (int x = 0; x < diameter; x++){
-      dX = abs(x - centX);
-      dist = sqrt(pow(dX, 2) + pow(dY, 2));
-      if(dist <= radius) {
-        kernel(x,y) = 1;
+
+
+std::vector<std::vector<int>> getBoxCoordinates(int * firstRows, int nRows){
+  // anciliary function to get the upper left and lower right coordinates of
+  // boxes. uses the first rows of the squared kernel which are occupied by the
+  // circular shape if the filter to create groups of rectangular boxes which
+  // start at the same line in the matrix. That is, the algorithm can make use
+  // of the integral image and gain in efficiency.
+  // This function returns a list of the upper left / lower right coordinates
+  // of the individual boxes
+  std::vector<std::vector<int>> ullr;
+
+  int prevVal;          // previous value of firstRows, needed for detecting change
+  int groupStartX = 0;  // remember the column in which the current group started
+  std::vector<int> box;  // initialize vector for ullr coordinates of group
+
+
+  for (int x = 0; x <= nRows; x++) {
+    // at the first iteration set up group variable and remember the start of the
+    // group in x direction
+    if (x == 0){
+      prevVal = firstRows[x];
+      groupStartX = x;
+    } else {
+      // if the firstrow does not change (i.e. the kernel starts at the same
+      // line as in the column before), just go on.
+
+      if (firstRows[x] != prevVal){
+      // if the kernel changes the row, create a new box for the group (i.e.
+      // rectangle box) which has just finished.
+
+        box.push_back(groupStartX); // left
+        box.push_back(prevVal); // upper
+        box.push_back(x - 1); // right
+        box.push_back(nRows - 1 - prevVal); // lower
+
+        // add the box to the the output
+        ullr.push_back(box);
+        box.clear();
+
+        // reassign prevVal to currenct group's firstRow's
+        prevVal = firstRows[x];
+        // and save x position
+        groupStartX = x;
       }
     }
   }
+
+  return ullr;
 }
 
 
 // [[Rcpp::export]]
-NumericMatrix intFocalMean(NumericMatrix rasterband, int diameter) {
+NumericMatrix intRadialFocalMean(NumericMatrix rasterband, int diameter) {
+  // Implementation of a radial focal mean algorithm using itegral raster images.
+  // This implementaiton is advantagous to classical focal operators, as it is
+  // more efficient especially with large kernels.
+  // With radial filters, one probably has to sacrifice some efficiency, though.
 
   // create output raster band
   int rows = rasterband.nrow();
@@ -107,38 +166,71 @@ NumericMatrix intFocalMean(NumericMatrix rasterband, int diameter) {
   NumericMatrix kernel(diameter, diameter);
   fillKernel(kernel, diameter);
 
+  // count number of pixels in kernel; needed for creating the mean
+  int nPix = 0;
+  for (int i = 0; i < diameter; i++)
+    for (int j = 0; j < diameter; j++)
+      if (kernel(i,j) == 1) nPix++;
+
   // find first rows in each column which are part of the disc kernel
   int firstRows[diameter];
   fillFirstRows(firstRows, kernel, diameter);
 
-  for (auto x : firstRows) std::cout << x << " ";
-  std::cout << std::endl;
+  // std::cout << "Firstrows: ";
+  // for (auto x : firstRows) std::cout << x << " ";
+  // std::cout << std::endl;
 
+  // find rectangular boxes in the disc shaped kernel to speed up processing
+  std::vector<std::vector<int>> ullr = getBoxCoordinates(firstRows, diameter);
+
+  // convert ullr coordinates to values relative to the centering pixel of the kernel
+  for (int x = 0; x < ullr.size(); x++) {
+    for (int y = 0; y < ullr[x].size(); y++) {
+      ullr[x][y] = ullr[x][y] - ((diameter - 1) / 2);
+    }
+  }
+
+
+  // from here on copied and adapted from original velox/src/focal.cpp
+  std::vector<double> v;
+  int sum;
+
+  int idim = (diameter-1)/2;
+  int jdim = (diameter-1)/2;
+
+  for (int i = 0; i < rows; i++) {
+    int imin = i - idim;
+    int imax = i + idim + 1;
+    for (int j = 0; j < cols; j++) {
+      int jmin = j - jdim;
+      int jmax = j + jdim + 1;
+
+      // for (int wi = imin; wi < imax; wi++) {
+      //   for (int wj = jmin; wj < jmax; wj++) {
+      //     if (wi >= 0 && wi < rows && wj >= 0 && wj < cols) {
+      //       // v.push_back(rasterband(wi, wj));
+      //     }
+      //   }
+      // }
+
+      sum = accumulate(v.begin(),v.end(),0);
+
+
+      newband(i,j) = sum / nPix;;
+      v.clear();
+    }
+  }
   return kernel;
-
-  // for (int r = 0; r < rows; r++){
-  //   for (int c = 0; c < cols; c++){
-  //
-  //
-  //
-  //   }
-  // }
-  // return  rasterband;
+  // return newband;
 }
 
-
-std::list<int> getBoxCoordinates(int firstRows[]){
-  std::list<int> ullr;
-
-  return ullr;
-}
 
 /*** R
 m <- matrix(c(0), nrow = 9, ncol = 9, byrow = TRUE)
 m[5,5] <- 1
 
 (i <- integralRaster(m))
-(imn <- intFocalMean(i, 9))
+(imn <- intRadialFocalMean(i, 9))
 raster::plot(raster::raster(imn))
 # largeM <- matrix(runif(100000000), nrow = 10000)
 # largeMint <- integralRaster(largeM)
